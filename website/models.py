8db21d81feefe4cae10d951fa5339494e5583295
Sidkjr <siddhant.khare.sk@gmail.com>
2022-11-18 18:49:14 +0530

    Fixed the relationships for the commentlike model

--- website/models.py
+++ website/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='user', passive_deletes=True)
     comments = db.relationship('Comment', backref='user', passive_deletes=True)
     likes = db.relationship('Like', backref='user', passive_deletes=True)
-    commentlikes = db.relationship('CommentLike', backref='user', passive_deletes=True)
+    commentlikes = db.relationship('Commentlike', backref='user', passive_deletes=True)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,14 +32,14 @@ class Comment(db.Model):
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
-    commentlikes = db.Column(db.Integer, db.ForeignKey('commentlike.id', ondelete="CASCADE"), nullable=False)
+    commentlikes = db.relationship('Commentlike', backref='comment', passive_deletes=True)
 
 class Like(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
 
-class CommentLike(db.Model):
+class Commentlike(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     comment_id = db.Column(db.Integer, db.ForeignKey('comment.id', ondelete="CASCADE"), nullable=False)
